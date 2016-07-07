b14c96c6ec787614e20eee3a5fe885016d3e152d
willi684.purdue.edu <willi684.purdue.edu>
2016-07-07 16:24:10 -0400

    Getting Info from the Movie Database API
    Built URI and got information back (popular movies). Added permission to
    access internet. Also created class to store information for each
    displayed movie (may not keep). Added refresh button (temporarily),
    which when pressed, makes the initial call to the API.

---
+++ PopularMovies/app/src/main/java/com/udacity/caraher/emma/popularmovies/MovieClass.java
@@ -0,0 +1,41 @@
+package com.udacity.caraher.emma.popularmovies;
+
+/**
+ * Created by Jemma on 7/7/2016.
+ */
+public class MovieClass {
+
+    public String originalTitle;
+    public String plotSynopsis;
+    public double userRating;
+    public String releaseDate;
+    public String posterPath;
+
+    public MovieClass(String title, String plot, double rating, String date, String path) {
+        originalTitle = title;
+        plotSynopsis = plot;
+        userRating = rating;
+        releaseDate = date;
+        posterPath = path;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public double getUserRating() {
+        return userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+}
--- PopularMovies/app/src/main/java/com/udacity/caraher/emma/popularmovies/PosterFragment.java
+++ PopularMovies/app/src/main/java/com/udacity/caraher/emma/popularmovies/PosterFragment.java
@@ -1,14 +1,26 @@
 package com.udacity.caraher.emma.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class PosterFragment extends Fragment {
 
@@ -18,6 +30,27 @@ public class PosterFragment extends Fragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.movies_fragment, menu);
+    }
+
+    public void updateWeather() {
+        (new FetchMoviesTask()).execute();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            updateWeather();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -41,5 +74,82 @@ public class PosterFragment extends Fragment {
         /* END from https://developer.android.com/guide/topics/ui/layout/gridview.html */
 
         return rootView;
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, String[]> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+
+                String baseUrl = "https://api.themoviedb.org/3/discover/movie?";
+                Uri builtUri = Uri.parse(baseUrl).buildUpon()
+                        .appendQueryParameter("api_key", "8d5d5aaec6797f2b46352b8844d64f6f")
+                        .appendQueryParameter("sort_by", "popularity.desc")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+                Log.v(LOG_TAG, moviesJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
