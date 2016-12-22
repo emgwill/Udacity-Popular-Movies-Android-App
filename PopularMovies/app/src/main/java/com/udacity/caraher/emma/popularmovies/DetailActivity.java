91b3a5168da4cb89934af388c98538166e43bb9c
willi684.purdue.edu <willi684.purdue.edu>
2016-12-22 07:36:58 -0500

    Fixed button bug

--- PopularMovies/app/src/main/java/com/udacity/caraher/emma/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/udacity/caraher/emma/popularmovies/DetailActivity.java
@@ -47,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.detail_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DetailFragment())
